1d7203ff9811c7d0857956790b887aca7b342323
Andrew <andrewgall12@gmail.com>
2022-11-01 15:49:54 -0400

    Making it a cra template

--- template/webpack.config.js
+++ template/webpack.config.js
@@ -1,4 +1,4 @@
 module.exports = async (env, argv) => {
-  const config = await import('./webpack.config.cjs');
+  const config = await import('./template/webpack.config.cjs');
   return config.default(env, argv);
 };
